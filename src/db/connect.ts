7c9c9aaa6ff8c87892253f87e86a793c6b757489
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-06-05 23:31:48 +0530

    added db connect

---
+++ src/db/connect.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import config from "config";
+import log from "../logger";
+
+const connect = () => {
+  const mongoURI = config.get("mongoURI") as string;
+
+  return mongoose
+    .connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      log.info("MongoDB is connected");
+    })
+    .catch((err) => {
+      log.error("Failed to connect MongoDB due to ERROR:", err);
+      process.exit(1);
+    });
+};
+
+export default connect;
