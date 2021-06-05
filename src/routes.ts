0cafcb20080038f3927af2c21d97cc6565fabc7d
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-06-06 00:25:09 +0530

    added api in route

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,7 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controllers/user.controller";
+import validateRequest from "./middlewares/validateRequest";
+import { createUserSchema } from "./schemas/user.schema";
 
 export default (app: Express) => {
   app.get("/healthcheck", (req: Request, res: Response) => {
@@ -6,6 +9,7 @@ export default (app: Express) => {
   });
 
   // Register user
+  app.post("/api/users", validateRequest(createUserSchema), createUserHandler);
 
   // Login User
 
