b725a6e35b221b059c58da1a23cdc92e20f7d8a4
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-06-08 17:55:33 +0530

    completed create access token function

--- src/services/session.service.ts
+++ src/services/session.service.ts
@@ -1,6 +1,9 @@
+import { LeanDocument } from "mongoose";
+import config from "config";
 import Session from "../models/session.model";
 import { SessionDocument } from "../models/session.model";
 import { UserDocument } from "../models/user.model";
+import { sign } from "../utils/jwt.util";
 
 export async function createSession(
   userId: SessionDocument["user"],
@@ -9,4 +12,23 @@ export async function createSession(
   const session = await Session.create({ user: userId, userAgent });
 
   return session.toJSON();
+}
+
+export function createAccessToken({
+  user,
+  session,
+}: {
+  user:
+    | Omit<UserDocument, "password">
+    | LeanDocument<Omit<UserDocument, "password">>;
+  session:
+    | Omit<SessionDocument, "password">
+    | LeanDocument<Omit<SessionDocument, "password">>;
+}) {
+  const accessToken = sign(
+    { ...user, session: session._id },
+    { expiresIn: config.get("accessTokenTTL") }
+  );
+
+  return accessToken;
 }
