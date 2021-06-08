05b87bf0a123c0c520207624f91eb8a9997476d8
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-06-08 18:31:42 +0530

    added createUserSessionSchema validation schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -7,10 +7,25 @@ export const createUserSchema = object({
       .required("Password is required")
       .min(6, "Password is too short - should be minimum 6 characters")
       .matches(/^[a-zA-Z0-9_.-]*$/, "Password can only contain Latin letters"),
+
     passwordConfirmation: string().oneOf(
       [ref("password"), null],
       "Passwords must match"
     ),
+
+    email: string()
+      .email("Must be a valid email address")
+      .required("Email is required"),
+  }),
+});
+
+export const createUserSessionSchema = object({
+  body: object({
+    password: string()
+      .required("Password is required")
+      .min(6, "Password is too short - should be minimum 6 characters")
+      .matches(/^[a-zA-Z0-9_.-]*$/, "Password can only contain Latin letters"),
+
     email: string()
       .email("Must be a valid email address")
       .required("Email is required"),
